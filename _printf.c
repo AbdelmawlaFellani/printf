02ccbc9c50d2de221b610b522b93af1169e61b73
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-24 13:36:20 +0000

    Abdelmawla: Setting up the _printf.c function empty as a start_up

---
+++ _printf.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _printf - Entry Point to the Function
+ *
+ * @format: char Input
+ *
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	/**
+	 * Code goes here 
+	 * Make sure to add comments to any changes accures
+	 * If you got any other conditions add them here...
+	 */
+
+}
