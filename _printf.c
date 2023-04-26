e2c048c7110f66acc4113d26fa880a7c1ae48594
AbdelmawlaFellani <fellaniabdo@gmail.com>
2023-04-26 00:19:25 +0000

    Abdelmawla: Task 0

--- _printf.c
+++ _printf.c
@@ -16,9 +16,9 @@ int str_len(char *s)
 		str_len("(null)");
 		return (6);
 	}
-	while (*s + i != '\0')
+	while (*(s + i) != '\0')
 	{
-		_putchar(*s + i);
+		_putchar(*(s + i));
 		i++;
 	}
 	return (i);
